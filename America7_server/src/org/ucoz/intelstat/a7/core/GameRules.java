44da666f8ecea99022d586175409f1f667a91925
InteLStaT <InteLStaT@users.noreply.github.com>
2017-08-31 20:55:13 +0300

    added support for all game rules
    excluding bugs

--- America7_server/src/org/ucoz/intelstat/a7/core/GameRules.java
+++ America7_server/src/org/ucoz/intelstat/a7/core/GameRules.java
@@ -1,21 +1,63 @@
 package org.ucoz.intelstat.a7.core;
 
-import org.ucoz.intelstat.a7.core.Game.Player;
 import org.ucoz.intelstat.gc.GCard;
 
+/**
+ * A game of America 7 is played counterclockwise.
+ * 
+ * @see Game
+ * @author InteLStaT
+ *
+ */
+// TODO: add proper javadoc
+// FIXME: if you're looking for a bug, it's likely here
 public class GameRules {
 
-	private GameRules() {}
-	
-	public static boolean isValidMove(GCard prevCard, GCard nextCard, Game game, Player player) {
-		if(nextCard.getRank() == GCard.SEVEN) {
+	private GameRules() {
+	}
+
+	/**
+	 * This method checks if playing the card {@code nextCard} is valid by the
+	 * rules of the game, not accounting SEVEN ranked cards.
+	 * 
+	 * @param prevCard
+	 *            the previous card (that is, the top card)
+	 * @param nextCard
+	 *            the card which is checked against {@code prevCard}
+	 * @param isStreak
+	 *            whether prevCard's rank (ACE OR UNDER) is in streak
+	 */
+	public static boolean isValidMove(GCard prevCard, GCard nextCard, boolean isStreak) {
+		/*
+		 * If the game is currently in (either an ACE or UNDER) streak, only
+		 * these ranks can be placed. The streak's rank is the previous card's
+		 * rank. NOTE: by the following code, and rank can streak, not just ACE
+		 * or UNDER.
+		 */
+		if (isStreak) {
+			if (nextCard.getRank() == prevCard.getRank()) {
+				return true;
+			}
+			return false;
+		}
+		if (nextCard.getRank() == GCard.SEVEN) {
 			return true;
 		}
-		if(prevCard.getSuit() == nextCard.getSuit() || prevCard.getRank() == nextCard.getRank()) {
+		if (prevCard.getRank() == nextCard.getRank() || prevCard.getSuit() == nextCard.getSuit()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isValidAskedCard(GCard card, GCard.Suit suit) {
+		if (card.getSuit() == suit) {
+			return true;
+		}
+		if (card.getRank() == GCard.Rank.SEVEN) {
 			return true;
 		}
 		return false;
 	}
-	
-	
+
 }
