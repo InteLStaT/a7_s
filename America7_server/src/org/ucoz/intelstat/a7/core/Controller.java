9a7b762a1bd3623d5ee061e88fec80a3086fd731
InteLStaT <InteLStaT@users.noreply.github.com>
2017-08-31 17:33:55 +0300

    changed GHand parameter to unmodifiable List<GCard>
    and removed init() because I found it useless

--- America7_server/src/org/ucoz/intelstat/a7/core/Controller.java
+++ America7_server/src/org/ucoz/intelstat/a7/core/Controller.java
@@ -1,8 +1,9 @@
 package org.ucoz.intelstat.a7.core;
 
+import java.util.List;
+
 import org.ucoz.intelstat.a7.core.Game.Player;
 import org.ucoz.intelstat.gc.GCard;
-import org.ucoz.intelstat.gc.GHand;
 
 public interface Controller {
 
@@ -10,10 +11,12 @@ public interface Controller {
 	 * Callback method, called when it's the associated player's 
 	 * turn. This method either has to return a card, or return 
 	 * null if the player wants to draw a card from the deck.
-	 * If the returned card 
+	 * 
 	 * @param topCard the top card in the pile, on top of which 
 	 * the next card will be placed
+	 * @param handView the list of cards in the controlled player's hand
+	 * @param askedFor the suit the a previous player has asked for, {@code null} if not asked
+	 * @param player the controlled player
 	 */
-	GCard proposeCard(GCard topCard, GHand hand, Game game, Player player);
-	default void init(Game.Player player) {}
+	GCard proposeCard(GCard topCard, List<GCard> handView, Game game, Player player);
 }
