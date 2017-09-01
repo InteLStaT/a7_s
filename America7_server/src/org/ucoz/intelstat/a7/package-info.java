e4e784055a8a770fc59013974e654c751c1de0b8
InteLStaT <InteLStaT@users.noreply.github.com>
2017-09-01 19:21:56 +0300

    added package info
    not very useful

---
+++ America7_server/src/org/ucoz/intelstat/a7/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Core classes for making an America 7 game possible. No networking done.
+ * <p>The code is very badly commented.
+ *
+ * @author InteLStaT
+ *
+ */
+package org.ucoz.intelstat.a7;
